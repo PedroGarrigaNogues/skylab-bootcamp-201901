e45050b3457720cf403aa819e76d27d094f0d2ae
Pedro <pe->
2019-02-19 13:45:44 +0100

    finished chat

--- staff/pedro-garriganogues/tcp-chat/server.js
+++ staff/pedro-garriganogues/tcp-chat/server.js
@@ -4,8 +4,9 @@ const net = require('net')
 
 const { argv: [, , host, from, message] } = process
 
-const [ip, port] = host.split(':')
+const [host, port] = adress.split(':')
 
-const conn = net.createConnection(port, ip)
-conn.end(`${from}: ${message}`)
+const conn = net.createConnection(port, host)
+conn.write(`${from}: ${message}`)
 conn.on('data', data => console.log(data.toString()))
+
